27374e59ac624bc05ff290326a199733f178244b
Kevin <>
2021-01-11 16:13:21 -0500

    Starting files for newsletter signup page

---
+++ app.js
@@ -0,0 +1,9 @@
+//jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+app.listen(3000, function(){
+  ("Server is running on port 3000.");
+});
