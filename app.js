e51130a50d4ff7cd142b3f5324745436a1c028a4
Kevin <>
2021-01-12 14:29:00 -0500

    First commit

--- app.js
+++ app.js
@@ -4,6 +4,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 
-app.listen(3000, function(){
-  ("Server is running on port 3000.");
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
 });
+
+app.post("/", function(req, res){
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  console.log(firstName, lastName, email);
+
+});
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000.");
+});
+
+//API Key: 2ae39da5740abbf67e09325388224d2f-us7
+//List ID: b22331592e
